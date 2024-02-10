06512591484ce0d63483a9b39cc070127b388656
MgNuu <rkdalsdn7@gmail.com>
2024-02-10 23:41:26 +0900

    kakao MapView 호출 성공

--- AOS/app/src/main/java/org/softeer/robocar/MapActivity.kt
+++ AOS/app/src/main/java/org/softeer/robocar/MapActivity.kt
@@ -2,19 +2,43 @@ package org.softeer.robocar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapReadyCallback
 import com.kakao.vectormap.MapView
+import org.softeer.robocar.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
-    lateinit var mapView: MapView
+    private lateinit var binding: ActivityMapBinding
+    private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MapFragment.newInstance())
-                .commitNow()
-        }
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_map)
+
+        mapView = binding.mapView
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출됨
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+                println(error)
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                // 인증 후 API가 정상적으로 실행될 때 호출됨
+            }
+        })
+
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MapFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
