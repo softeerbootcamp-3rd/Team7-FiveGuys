2a84e2c16225ad898d0d86f4cc315d20c02de324
StrangeMin <mim0107@nate.com>
2024-02-10 21:38:39 +0900

    [Feat] 완료 버튼 클릭 시, editText의 focus를 clear하는 기능 추가

--- AOS/app/src/main/java/org/softeer/robocar/ui/custom/BackPressClearFocusEditText.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/custom/BackPressClearFocusEditText.kt
@@ -3,6 +3,7 @@ package org.softeer.robocar.ui.custom
 import android.content.Context
 import android.util.AttributeSet
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.widget.AppCompatEditText
 
 class BackPressClearFocusEditText : AppCompatEditText {
@@ -10,6 +11,15 @@ class BackPressClearFocusEditText : AppCompatEditText {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    init {
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                clearFocus()
+            }
+            false
+        }
+    }
 
     override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
