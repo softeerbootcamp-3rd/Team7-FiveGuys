f36c17d615069cd414d743b631a907afbeca977a
StrangeMin <mim0107@nate.com>
2024-02-11 23:59:13 +0900

    [Feat] 동승 요청(발신) 다이얼로그 레이아웃 구현

---
+++ AOS/app/src/main/java/org/softeer/robocar/ui/fragment/CarPoolRequestDialogFragment.kt
@@ -0,0 +1,32 @@
+package org.softeer.robocar.ui.fragment
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.softeer.robocar.databinding.FragmentDialogCarPoolRequestBinding
+
+class CarPoolRequestDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentDialogCarPoolRequestBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentDialogCarPoolRequestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.cancelRequestButton.setOnClickListener {
+            // TODO API 연결
+            dismiss()
+        }
+    }
+}
