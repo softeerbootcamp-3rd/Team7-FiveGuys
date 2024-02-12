236f2a7e444eb55c4aa7239ff1fb1fe62a196e23
MgNuu <rkdalsdn7@gmail.com>
2024-02-12 19:51:25 +0900

    [Feat] HeadcountDialogFragment 제작

---
+++ AOS/app/src/main/java/org/softeer/robocar/ui/fragment/HeadcountDialogFragment.kt
@@ -0,0 +1,23 @@
+package org.softeer.robocar.ui.fragment
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.softeer.robocar.R
+
+class HeadcountDialogFragment : DialogFragment(R.layout.fragment_headcount_dialog) {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+
+            builder.setView(R.layout.fragment_headcount_dialog)
+            builder.create()
+        }?: throw IllegalStateException("Activity cannot be null")
+    }
+}
