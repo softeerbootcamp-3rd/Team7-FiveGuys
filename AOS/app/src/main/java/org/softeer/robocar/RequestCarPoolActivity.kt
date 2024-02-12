72f7c5698a7e7f48f75c4adcaee0795c715cae4f
StrangeMin <mim0107@nate.com>
2024-02-12 17:20:50 +0900

    [Feat] requestCarPool 공통 레이아웃 구현

---
+++ AOS/app/src/main/java/org/softeer/robocar/RequestCarPoolActivity.kt
@@ -0,0 +1,11 @@
+package org.softeer.robocar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RequestCarPoolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_car_pool)
+    }
+}
