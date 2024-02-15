80dcbf4a293c14866338ce8cc17862700a87b157
yo0919 <0919yo@naver.com>
2024-02-15 18:50:45 +0900

    [Feat] 주소를 좌표값으로 변환하는 기능 추가
    - MapService에 지번,도로명주소등 상세 주소를 받으면 좌표로 변환하는 로직 추가

--- BE/robocar/src/main/java/com/fiveguys/robocar/service/MapService.java
+++ BE/robocar/src/main/java/com/fiveguys/robocar/service/MapService.java
@@ -2,12 +2,17 @@ package com.fiveguys.robocar.service;
 
 import com.fiveguys.robocar.apiPayload.ResponseStatus;
 import com.fiveguys.robocar.apiPayload.exception.GeneralException;
+import com.fiveguys.robocar.util.JsonParserUtil;
+import com.fiveguys.robocar.util.JsonParserUtil.Coordinate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriUtils;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class MapService {
@@ -35,6 +40,24 @@ public class MapService {
             return restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         } catch (RestClientException e) {
             // 새로운 ResponseStatus를 사용하여 GeneralException을 던집니다.
+            throw new GeneralException(ResponseStatus.EXTERNAL_SERVICE_ERROR);
+        }
+    }
+    public Coordinate convertAddressToCoordinates(String address) {
+        String url = String.format("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=%s", address);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-NCP-APIGW-API-KEY-ID", apiKeyId);
+        headers.set("X-NCP-APIGW-API-KEY", apiKeySecret);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            // 받은 JSON에서 x와 y 값을 추출
+            Coordinate coordinate = JsonParserUtil.extractCoordinatesFromAddressResponse(response.getBody());
+            return coordinate;
+        } catch (RestClientException e) {
             throw new GeneralException(ResponseStatus.EXTERNAL_SERVICE_ERROR);
         }
     }
--- BE/robocar/src/test/java/com/fiveguys/robocar/service/MapServiceTest.java
+++ BE/robocar/src/test/java/com/fiveguys/robocar/service/MapServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fiveguys.robocar.util.JsonParserUtil.Coordinate;
 
 
 @SpringBootTest
@@ -17,7 +18,7 @@ public class MapServiceTest {
     private ObjectMapper objectMapper; // ObjectMapper 주입
 
     @Test
-    public void testGetRoute() throws Exception { // 예외 처리 추가
+    public void testGetRoute() throws Exception {
         String start = "127.1058342,37.359708";
         String goal1 = "127.1230000,37.323";
         //String goal2 = "127.1156,37.3456";
@@ -27,5 +28,17 @@ public class MapServiceTest {
         // 응답 본문을 JSON 문자열로 변환하여 출력
         String jsonResponse = objectMapper.writeValueAsString(responseEntity.getBody());
         System.out.println("응답 JSON: " + jsonResponse);
+    }
+
+    @Test
+    public void testConvertAddressToCoordinates() {
+        // 주소 예시: "분당구 불정로 6"
+        String address = "분당구 불정로 6";
+        Coordinate coordinate = mapService.convertAddressToCoordinates(address);
+
+        // 좌표 출력
+        System.out.println("Converted Coordinates:");
+        System.out.println("Latitude: " + coordinate.getLatitude());
+        System.out.println("Longitude: " + coordinate.getLongitude());
     }
 }
