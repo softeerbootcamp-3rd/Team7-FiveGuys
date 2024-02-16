f355b5ae1ca892d1f6f5ef2be5dc2ed43776cbf5
MgNuu <rkdalsdn7@gmail.com>
2024-02-16 18:43:53 +0900

    [Refactor] bottom sheet 내부를 fragment container로 변경, fragment 생성

--- AOS/app/src/main/java/org/softeer/robocar/ui/activity/MapActivity.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/activity/MapActivity.kt
@@ -3,12 +3,16 @@ package org.softeer.robocar.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup.MarginLayoutParams
+import android.view.inputmethod.EditorInfo
 import androidx.activity.enableEdgeToEdge
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
@@ -17,24 +21,33 @@ import com.kakao.vectormap.MapView
 import org.softeer.robocar.R
 import org.softeer.robocar.databinding.ActivityMapBinding
 import org.softeer.robocar.ui.fragment.HeadcountDialogFragment
+import org.softeer.robocar.ui.fragment.PathSettingFragment
 
 class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
     private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.updateLayoutParams<MarginLayoutParams> {
-                bottomMargin = insets.bottom
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<PathSettingFragment>(R.id.destinationFragmentContainer)
             }
-
-            WindowInsetsCompat.CONSUMED
         }
+
+//        status bar를 투명화하는 코드, bottom sheet와 충돌이 있어서 주석 처리함
+//        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+//            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.updateLayoutParams<MarginLayoutParams> {
+//                bottomMargin = insets.bottom
+//            }
+//
+//            WindowInsetsCompat.CONSUMED
+//        }
 
         mapView = binding.mapView
         mapView.start(object : MapLifeCycleCallback() {
@@ -51,8 +64,7 @@ class MapActivity : AppCompatActivity() {
             }
         })
 
-        var behavior = BottomSheetBehavior<ConstraintLayout>()
-        behavior = BottomSheetBehavior.from(binding.selectDestination)
+        val behavior = BottomSheetBehavior.from(binding.destinationLayout)
 
         HeadcountDialogFragment().show(supportFragmentManager, "headCount")
     }
