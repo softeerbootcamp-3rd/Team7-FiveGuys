a449e90a341de2a3b0ba97162ab0be20a367f767
StrangeMin <mim0107@nate.com>
2024-02-18 13:55:14 +0900

    [Chore] 응답 DTO 수정

--- AOS/app/src/main/java/org/softeer/robocar/data/dto/carpool/response/CarPoolListResponse.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/dto/carpool/response/CarPoolListResponse.kt
@@ -1,16 +1,27 @@
 package org.softeer.robocar.data.dto.carpool.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CarPoolListResponse(
+    @SerialName("price")
+    val originalCharge: Int,
+    val list: List<CarPoolInformation>
+)
+
+@Serializable
+data class CarPoolInformation(
     val hostId: Long,
-    val nickname: String,
-    val departAltitude: Double,
+    val hostNickname: String,
+    val departLongitude: Double,
     val departLatitude: Double,
-    val hostDestAltitude: Double,
+    val hostDestLongitude: Double,
     val hostDestLatitude: Double,
     val hostDepartAddress: String,
     val hostDestAddress: String,
     val maleCount: Int,
     val femaleCount: Int,
-    val estimatedTime: String,
-    val estimatedPrice: String,
+    val estimatedTime: Int,
+    val estimatedPrice: Int,
 )
--- AOS/app/src/main/java/org/softeer/robocar/data/service/CarPool/CarPoolService.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/service/CarPool/CarPoolService.kt
@@ -1,14 +1,15 @@
 package org.softeer.robocar.data.service.CarPool
 
-import org.softeer.robocar.data.dto.carpool.request.CarPoolListRequest
 import org.softeer.robocar.data.dto.carpool.response.CarPoolListResponse
-import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CarPoolService {
 
     @GET("operations/carpools")
-    suspend fun getCarPoolList(@Body carPoolListRequest: CarPoolListRequest): Response<CarPoolListResponse>
+    suspend fun getCarPoolList(
+        @Query("guestDepartAddress") startLocation: String,
+        @Query("guestDestAddress") destinationLocation: String
+    ): CarPoolListResponse
 
 }
