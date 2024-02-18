a84528986cbb89605c1d446f6e29513c7974967b
eyeben <62013201+eyeben@users.noreply.github.com>
2024-02-19 04:15:07 +0900

    [Feat] Carpool관련 파일 생성
    컨트롤러와 Dto 생성, 기본적인 컨트롤러 구현

---
+++ BE/robocar/src/main/java/com/fiveguys/robocar/controller/CarpoolController.java
@@ -0,0 +1,75 @@
+package com.fiveguys.robocar.controller;
+
+
+import com.fiveguys.robocar.apiPayload.ResponseApi;
+import com.fiveguys.robocar.dto.req.CarpoolRegisterReqDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Carpool", description = "카풀리스트 - 등록,조회,삭제")
+public class CarpoolController {
+
+
+
+    @Operation(summary = "게스트 위치 기반 리스트 조회")
+    @Parameters(value = {
+            @Parameter(name = "latitude", description = "위도"),
+            @Parameter(name = "longitude", description = "경도"),
+
+
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "추가 성공")
+    })
+    @GetMapping("/operations/carpools")
+    public ResponseEntity carpoolListUp(@RequestParam String guestDepartAddress, @RequestParam String guestDestAddress){
+        return ResponseApi.ok();
+    }
+
+    @Operation(summary = "호스트가 카풀 등록")
+    @Parameters(value = {
+            @Parameter(name = "latitude", description = "위도"),
+            @Parameter(name = "longitude", description = "경도"),
+
+
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "추가 성공")
+    })
+    @PostMapping("/operations")
+    public ResponseEntity carpoolRegister(@RequestBody @Validated CarpoolRegisterReqDto carpoolRegisterReqDto){
+        return ResponseApi.ok();
+    }
+
+    @Operation(summary = "호스트가 카풀 등록")
+    @Parameters(value = {
+            @Parameter(name = "latitude", description = "위도"),
+            @Parameter(name = "longitude", description = "경도"),
+
+
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "추가 성공")
+    })
+    @DeleteMapping("/operations")
+    public ResponseEntity carpoolDelete(HttpServletRequest request){
+        return ResponseApi.ok();
+    }
+
+
+
+
+
+
+}
---
+++ BE/robocar/src/main/java/com/fiveguys/robocar/dto/req/CarpoolRegisterReqDto.java
@@ -0,0 +1,12 @@
+package com.fiveguys.robocar.dto.req;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CarpoolRegisterReqDto {
+
+
+}
