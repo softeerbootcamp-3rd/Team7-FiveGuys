661edd95b4d886345db30347857de3f1f1e964f7
yo0919 <0919yo@naver.com>
2024-02-19 14:55:29 +0900

    [Feat] NaverMapConfig추가

--- BE/robocar/src/main/java/com/fiveguys/robocar/service/MapService.java
+++ BE/robocar/src/main/java/com/fiveguys/robocar/service/MapService.java
@@ -2,10 +2,10 @@ package com.fiveguys.robocar.service;
 
 import com.fiveguys.robocar.apiPayload.ResponseStatus;
 import com.fiveguys.robocar.apiPayload.exception.GeneralException;
+import com.fiveguys.robocar.config.NaverMapConfig;
 import com.fiveguys.robocar.util.JsonParserUtil;
 import com.fiveguys.robocar.util.JsonParserUtil.Coordinate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
@@ -17,48 +17,48 @@ import java.nio.charset.StandardCharsets;
 @Service
 public class MapService {
     private final RestTemplate restTemplate;
-    private final String apiKeyId;
-    private final String apiKeySecret;
+    private final NaverMapConfig naverMapConfig;
 
     @Autowired
-    public MapService(RestTemplate restTemplate, @Value("${naver.api.key.id}") String apiKeyId, @Value("${naver.api.key.secret}") String apiKeySecret) {
+    public MapService(RestTemplate restTemplate, NaverMapConfig naverMapConfig) {
         this.restTemplate = restTemplate;
-        this.apiKeyId = apiKeyId;
-        this.apiKeySecret = apiKeySecret;
+        this.naverMapConfig = naverMapConfig;
     }
 
     public ResponseEntity<String> getRoute(String start, String goal1, String goal2) {
         String waypointParam = goal2 != null && !goal2.isEmpty() ? "&waypoints=" + goal2 : "";
-        String url = String.format("https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving?start=%s&goal=%s%s&option=trafast", start.replace(" ", ""), goal1.replace(" ", ""), waypointParam.replace(" ", ""));
+        String url = String.format("%s?start=%s&goal=%s%s&option=trafast",
+                naverMapConfig.DIRECTION_API_URL, UriUtils.encodeQueryParam(start, StandardCharsets.UTF_8), UriUtils.encodeQueryParam(goal1, StandardCharsets.UTF_8), UriUtils.encodeQueryParam(waypointParam, StandardCharsets.UTF_8));
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-NCP-APIGW-API-KEY-ID", apiKeyId);
-        headers.set("X-NCP-APIGW-API-KEY", apiKeySecret);
+        HttpHeaders headers = createHeaders();
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         try {
             return restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         } catch (RestClientException e) {
-            // 새로운 ResponseStatus를 사용하여 GeneralException을 던집니다.
             throw new GeneralException(ResponseStatus.EXTERNAL_SERVICE_ERROR);
         }
     }
-    public Coordinate convertAddressToCoordinates(String address) {
-        String url = String.format("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=%s", address);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("X-NCP-APIGW-API-KEY-ID", apiKeyId);
-        headers.set("X-NCP-APIGW-API-KEY", apiKeySecret);
+    public Coordinate convertAddressToCoordinates(String address) {
+        String url = String.format("%s?query=%s", naverMapConfig.GEOCODE_API_URL,address);
+
+        HttpHeaders headers = createHeaders();
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         try {
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            // 받은 JSON에서 x와 y 값을 추출
-            Coordinate coordinate = JsonParserUtil.extractCoordinatesFromAddressResponse(response.getBody());
-            return coordinate;
+            return JsonParserUtil.extractCoordinatesFromAddressResponse(response.getBody());
         } catch (RestClientException e) {
             throw new GeneralException(ResponseStatus.EXTERNAL_SERVICE_ERROR);
         }
+    }
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-NCP-APIGW-API-KEY-ID", naverMapConfig.getApiKeyId());
+        headers.set("X-NCP-APIGW-API-KEY", naverMapConfig.getApiKeySecret());
+        return headers;
     }
 }
