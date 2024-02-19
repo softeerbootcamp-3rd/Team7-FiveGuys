5ecad19275e6fdd93fd66a0eb506f46497db842c
eyeben <62013201+eyeben@users.noreply.github.com>
2024-02-20 01:36:38 +0900

    [Refractor] 새로운 JWT에 맞추어 리펙토링
    새로 업데이트 된 JWT에 맞도록 컨트롤러와 서비스 리펙토링. 회원가입 부분은 인증오류 발생

--- BE/robocar/src/main/java/com/fiveguys/robocar/dto/req/UserCreateReqDto.java
+++ BE/robocar/src/main/java/com/fiveguys/robocar/dto/req/UserCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.fiveguys.robocar.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,11 +11,14 @@ import lombok.Getter;
 public class UserCreateReqDto {
     @Size(max = 50)
     @NotBlank
+    @Schema(description = "로그인아이디")
     private String loginId;
     @Size(max = 50)
     @NotBlank
+    @Schema(description = "비밀번호")
     private String password;
     @Size(max = 50)
     @NotBlank
+    @Schema(description = "닉네임")
     private String nickname;
 }
