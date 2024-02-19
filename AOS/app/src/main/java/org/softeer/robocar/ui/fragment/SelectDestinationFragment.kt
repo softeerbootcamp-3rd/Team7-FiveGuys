3d86d9805d0e4063af0a9463623c81f8b64affae
MgNuu <rkdalsdn7@gmail.com>
2024-02-20 00:10:05 +0900

    [Feat] InternalControlFragment 제작(차량 내부 원격 조종 바텀 시트)

--- AOS/app/src/main/java/org/softeer/robocar/ui/fragment/SelectDestinationFragment.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/fragment/SelectDestinationFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import org.softeer.robocar.R
+import org.softeer.robocar.databinding.FragmentPathSettingBinding
+import org.softeer.robocar.databinding.FragmentSelectDestinationBinding
 
 class SelectDestinationFragment : Fragment() {
+    private var _binding: FragmentSelectDestinationBinding?= null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_destination, container, false)
+    ): View {
+        _binding = FragmentSelectDestinationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.finishSelectDest.setOnClickListener {
+            findNavController().navigate(R.id.action_selectDestinationFragment_to_internalControlFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
