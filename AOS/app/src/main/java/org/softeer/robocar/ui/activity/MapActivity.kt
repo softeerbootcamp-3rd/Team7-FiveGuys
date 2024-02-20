c917dedbfae9642627a91c50cca76eb74eada7d6
yo0919 <0919yo@naver.com>
2024-02-20 16:51:59 +0900

    [Chore] dev브랜치 머지

--- AOS/app/src/main/java/org/softeer/robocar/ui/activity/MapActivity.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/activity/MapActivity.kt
@@ -2,7 +2,18 @@ package org.softeer.robocar.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.MarginLayoutParams
+import android.view.inputmethod.EditorInfo
+import androidx.activity.enableEdgeToEdge
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.MapLifeCycleCallback
@@ -10,14 +21,26 @@ import com.kakao.vectormap.MapView
 import org.softeer.robocar.R
 import org.softeer.robocar.databinding.ActivityMapBinding
 import org.softeer.robocar.ui.fragment.HeadcountDialogFragment
+import org.softeer.robocar.ui.fragment.PathSettingFragment
 
 class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
     private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
+
+//        status bar를 투명화하는 코드, bottom sheet와 충돌이 있어서 주석 처리함
+//        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+//            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.updateLayoutParams<MarginLayoutParams> {
+//                bottomMargin = insets.bottom
+//            }
+//
+//            WindowInsetsCompat.CONSUMED
+//        }
 
         mapView = binding.mapView
         mapView.start(object : MapLifeCycleCallback() {
@@ -33,6 +56,8 @@ class MapActivity : AppCompatActivity() {
                 // 인증 후 API가 정상적으로 실행될 때 호출됨
             }
         })
+
+        val behavior = BottomSheetBehavior.from(binding.destinationLayout)
 
         HeadcountDialogFragment().show(supportFragmentManager, "headCount")
     }
