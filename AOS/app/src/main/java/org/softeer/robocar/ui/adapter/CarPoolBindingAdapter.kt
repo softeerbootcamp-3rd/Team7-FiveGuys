4e666d4b52362cc7d4a0b1d47d0bf5ae6123b732
StrangeMin <mim0107@nate.com>
2024-02-20 11:20:07 +0900

    [Feat] 타입 선택 라디오 버튼 구현

--- AOS/app/src/main/java/org/softeer/robocar/ui/adapter/CarPoolBindingAdapter.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/adapter/CarPoolBindingAdapter.kt
@@ -1,10 +1,15 @@
 package org.softeer.robocar.ui.adapter
 
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.airbnb.lottie.LottieAnimationView
+import org.softeer.robocar.R
+import org.softeer.robocar.data.model.CarPoolType
 import org.softeer.robocar.data.model.CarPools
+import org.softeer.robocar.data.model.TaxiType
 import org.softeer.robocar.utils.convertMinutesToHoursAndMinutes
 import org.softeer.robocar.utils.formatDurationText
 import java.text.DecimalFormat
@@ -27,7 +32,7 @@ fun setExpectedCharge(textView: AppCompatTextView, originalCharge: Int, expected
     textView.text = formattedText
 }
 
-@BindingAdapter("setCountOfMale" , "setCountOfFemale")
+@BindingAdapter("setCountOfMale", "setCountOfFemale")
 fun setCountOfPassenger(textView: AppCompatTextView, countOfMale: Int, countOfFemale: Int) {
     val maleText = countOfMale.let { "남성 $it 명" }
     val femaleText = countOfFemale.let { "여성 $it 명" }
@@ -43,13 +48,73 @@ fun setCountOfPassenger(textView: AppCompatTextView, countOfMale: Int, countOfFe
 }
 
 @BindingAdapter("setCountOfCarPoolList")
-fun setCountOfCarPoolList(textView: AppCompatTextView, availableCarPoolCount: Int){
-    if(availableCarPoolCount != 0)
+fun setCountOfCarPoolList(textView: AppCompatTextView, availableCarPoolCount: Int) {
+    if (availableCarPoolCount != 0)
         textView.text = "총 ${availableCarPoolCount}대의 동승 가능한 차량을 찾았어요"
 }
 
 @BindingAdapter("setAnimationVisibilityAsGone")
-fun setAnimationVisibilityAsGone(animationView: LottieAnimationView, carPools : CarPools?){
-    if(carPools != null)
+fun setAnimationVisibilityAsGone(animationView: LottieAnimationView, carPools: CarPools?) {
+    if (carPools != null)
         animationView.visibility = View.GONE
+}
+
+@BindingAdapter("setTaxiTypeRadioButton")
+fun setTaxiTypeRadioButton(button: AppCompatImageButton, taxiType: TaxiType) {
+    if (taxiType == TaxiType.COMPACT_TAXI) {
+        if (button.id == R.id.compactTaxiButton) {
+            button.setImageResource(R.drawable.img_compact_taxi)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_hyundai_blue_radius_30)
+        } else if (button.id == R.id.midsizeTaxiButton) {
+            button.setImageResource(R.drawable.img_midsize_taxi_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        }
+    } else if (taxiType == TaxiType.MID_SIZE_TAXI) {
+        if (button.id == R.id.midsizeTaxiButton) {
+            button.setImageResource(R.drawable.img_midsize_taxi)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_sky_blue_pms_radius_30)
+        } else if (button.id == R.id.compactTaxiButton) {
+            button.setImageResource(R.drawable.img_compact_taxi_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        }
+    }
+}
+
+@BindingAdapter("setCarPoolTypeRadioButton")
+fun setCarPoolTypeRadioButton(button: AppCompatImageButton, carPoolType: CarPoolType) {
+    if (carPoolType == CarPoolType.ALONE) {
+        if (button.id == R.id.rideAloneButton) {
+            button.setImageResource(R.drawable.img_alone_riding)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_navy_radius_30)
+        } else if (button.id == R.id.participateCarPoolButton) {
+            button.setImageResource(R.drawable.img_participate_car_pool_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        } else if(button.id == R.id.recruitCarPoolButton){
+            button.setImageResource(R.drawable.img_recruit_car_pool_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        }
+    } else if (carPoolType == CarPoolType.RECRUIT) {
+        if (button.id == R.id.rideAloneButton) {
+            button.setImageResource(R.drawable.img_alone_riding_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        } else if (button.id == R.id.participateCarPoolButton) {
+            button.setImageResource(R.drawable.img_participate_car_pool_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        } else if(button.id == R.id.recruitCarPoolButton){
+            button.setImageResource(R.drawable.img_recruit_car_pool)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_yellow_200_radius_30)
+        }
+    } else if (carPoolType == CarPoolType.JOIN) {
+        if (button.id == R.id.rideAloneButton) {
+            button.setImageResource(R.drawable.img_alone_riding_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        } else if (button.id == R.id.participateCarPoolButton) {
+            button.setImageResource(R.drawable.img_participate_car_pool)
+            button.background =
+                ContextCompat.getDrawable(button.context, R.drawable.rectangle_active_blue_pms_312c_radius_30)
+        } else if (button.id == R.id.recruitCarPoolButton) {
+            button.setImageResource(R.drawable.img_recruit_car_pool_inactive)
+            button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_700_radius_30)
+        }
+    }
 }
