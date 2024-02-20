ab983d95b260c4eef259219cb23e59119c59362a
StrangeMin <mim0107@nate.com>
2024-02-21 03:49:41 +0900

    [Chore] Dto 필드명 수정

--- AOS/app/src/main/java/org/softeer/robocar/data/dto/user/response/CheckAvailableUserIdResponse.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/dto/user/response/CheckAvailableUserIdResponse.kt
@@ -1,5 +1,10 @@
 package org.softeer.robocar.data.dto.user.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CheckAvailableUserIdResponse(
+    @SerialName("available")
     val isAvailable: Boolean
 )
