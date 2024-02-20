6b301dd09fff18507d1a9046010413cf165763ca
StrangeMin <mim0107@nate.com>
2024-02-20 20:01:30 +0900

    [Chore] LoginRequest Dto 필드명 수정

--- AOS/app/src/main/java/org/softeer/robocar/data/dto/login/request/LoginRequest.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/dto/login/request/LoginRequest.kt
@@ -1,9 +1,11 @@
 package org.softeer.robocar.data.dto.login.request
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LoginRequest(
-    val loginId: String,
+    @SerialName("loginId")
+    val userId: String,
     val password: String,
 )
