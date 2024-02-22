90279aa601142eba3a969d842695cd697d7a89c4
StrangeMin <mim0107@nate.com>
2024-02-23 00:23:58 +0900

    [Chore] hiltViewModel 사용

--- AOS/app/src/main/java/org/softeer/robocar/ui/viewmodel/CarPoolListViewModel.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/viewmodel/CarPoolListViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.softeer.robocar.data.model.CarPools
 import org.softeer.robocar.domain.usecase.GetCarPoolListUseCase
 import javax.inject.Inject
 
+@HiltViewModel
 class CarPoolListViewModel @Inject constructor(
     private val getCarPoolListUseCase: GetCarPoolListUseCase
 ) : ViewModel() {
