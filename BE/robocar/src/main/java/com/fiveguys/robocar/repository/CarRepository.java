d23ddd42fa3970700aa2a4266c3d3f5f30d76d9a
yo0919 <0919yo@naver.com>
2024-02-23 21:57:45 +0900

    [Chore] Car Api 관련 수정

--- BE/robocar/src/main/java/com/fiveguys/robocar/repository/CarRepository.java
+++ BE/robocar/src/main/java/com/fiveguys/robocar/repository/CarRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-    Optional<Car> findByCarNumber(String carId);
+    Optional<Car> findByCarNumber(String carNumber);
     Optional<Car> findByGarageIdAndState(Long garageId, CarState state);
 }
 
