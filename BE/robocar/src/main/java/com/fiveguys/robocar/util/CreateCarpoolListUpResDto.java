9ab7313b5fdd16a62506a8b9b83cb6b8938aff74
eyeben <62013201+eyeben@users.noreply.github.com>
2024-02-24 00:24:47 +0900

    [Fix] 예외 처리 추가
    리스트업에서 지도상 가까울 경우 나는 에러를 해결

--- BE/robocar/src/main/java/com/fiveguys/robocar/util/CreateCarpoolListUpResDto.java
+++ BE/robocar/src/main/java/com/fiveguys/robocar/util/CreateCarpoolListUpResDto.java
@@ -53,13 +53,19 @@ public class CreateCarpoolListUpResDto {
         long taxifare;
         RouteInfo routeInfo;
 
+
         for(CarpoolRequest req : iterableRequests){
+            if(req == null)
+                continue;
 
             hostDepartCoordinate = String.format("%f,%f",req.getDepartLongitude(),req.getDepartLatitude());
 
             //10분 초과 거리는 제외
-            if(!hostDepartCoordinate.equals(guestDepartCoordinate) && routeService.getRouteInfo(hostDepartCoordinate, guestDepartCoordinate,null ).getDuration()/(1000*60) > MAX_RANGE_TIME)
-                continue;
+
+            try {
+                if (!hostDepartCoordinate.equals(guestDepartCoordinate) && routeService.getRouteInfo(hostDepartCoordinate, guestDepartCoordinate, null).getDuration() / (1000 * 60) > MAX_RANGE_TIME)
+                    continue;
+            } catch (Exception e){}
 
             //인원수 초과시 제외
             if(req.getFemaleCount()+req.getMaleCount()+guestMaleCount+guestFemaleCount > req.getCarType().getCapacity())
@@ -92,8 +98,10 @@ public class CreateCarpoolListUpResDto {
                     .estimatedPrice(taxifare)
                     .build();
             carpoolListUpResDto.addCarpoolItem(carpoolItem);
+
         }
 
+        System.out.println("ok");
         carpoolListUpResDto.doSortByEstimatedTime();
         carpoolListUpResDto.trimByLengthOf(MAX_LIST_LENGTH);
         return carpoolListUpResDto;
