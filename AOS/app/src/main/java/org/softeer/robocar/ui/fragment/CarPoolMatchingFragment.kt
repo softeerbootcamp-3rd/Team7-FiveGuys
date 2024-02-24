68272f79c983698425ce21b2ea5a0c68be92bfa1
StrangeMin <mim0107@nate.com>
2024-02-25 01:16:59 +0900

    [Feat] 요청 push 다이얼로그 구현

--- AOS/app/src/main/java/org/softeer/robocar/ui/fragment/CarPoolMatchingFragment.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/fragment/CarPoolMatchingFragment.kt
@@ -9,18 +9,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import org.softeer.robocar.R
 import org.softeer.robocar.databinding.FragmentCarPoolMatchingBinding
+import org.softeer.robocar.ui.viewmodel.CarPoolMatchingViewModel
 
+@AndroidEntryPoint
 class CarPoolMatchingFragment : Fragment() {
 
     private var _binding: FragmentCarPoolMatchingBinding? = null
     private val binding
         get() = _binding!!
 
+    private val viewModel: CarPoolMatchingViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentCarPoolMatchingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.registerCarPool()
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
+            messageReceiver, IntentFilter("org.softeer.robocar.NEW_MESSAGE")
+        )
+    }
+
     private val messageReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
+            val dialogIsShowing = findNavController().currentBackStackEntry?.destination?.id == R.id.receivedCarPoolRequestDialogFragment
+            if(dialogIsShowing)
+                return
+
             val maleCount = intent?.getStringExtra("maleCount") ?: "0"
             val femaleCount = intent?.getStringExtra("femaleCount") ?: "0"
             val guestId = intent?.getStringExtra("guestId") ?: "0"
@@ -36,17 +61,5 @@ class CarPoolMatchingFragment : Fragment() {
             )
             findNavController().navigate(action)
         }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentCarPoolMatchingBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
-            messageReceiver, IntentFilter("org.softeer.robocar.NEW_MESSAGE")
-        )
     }
 }
