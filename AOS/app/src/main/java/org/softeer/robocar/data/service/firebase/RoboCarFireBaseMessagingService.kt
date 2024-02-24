9ae409e5dd2942a39b34d45dc45c811d094e639e
StrangeMin <mim0107@nate.com>
2024-02-25 01:03:04 +0900

    [Feat] push 메시지 데이터 전달 로직 구현

--- AOS/app/src/main/java/org/softeer/robocar/data/service/firebase/RoboCarFireBaseMessagingService.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/service/firebase/RoboCarFireBaseMessagingService.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import kotlinx.serialization.json.Json
 
 class RoboCarFireBaseMessagingService : FirebaseMessagingService() {
 
@@ -15,8 +14,10 @@ class RoboCarFireBaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        val type = message.data["type"]
+        if(type == null)
+            return
 
-        // TODO 푸시알람 타입에 따라 다르게 보내기
         sendRequestCarPoolMessage(message)
     }
 
@@ -26,8 +27,15 @@ class RoboCarFireBaseMessagingService : FirebaseMessagingService() {
         intent.putExtra("guestId", message.data["guestId"])
         intent.putExtra("maleCount", message.data["maleCount"])
         intent.putExtra("femaleCount", message.data["femaleCount"])
-        intent.putExtra("guestAddress", message.data["guestAddress"])
-        intent.putExtra("nickname", message.data["nickname"])
+        intent.putExtra("guestAddress", message.data["guestDestAddress"])
+        intent.putExtra("nickname", message.data["guestNickname"])
+        intent.putExtra("type", message.data["type"])
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    companion object{
+        const val CAR_POOL_REJECT = "CAR_POOL_REJECT"
+        const val CAR_POOL_REQUEST = "CAR_POOL_REQUEST"
+        const val CAR_POOL_ACCEPT = "CAR_POOL_ACCEPT"
     }
 }
