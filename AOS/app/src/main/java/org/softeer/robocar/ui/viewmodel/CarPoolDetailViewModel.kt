557e40ec0ef95e124be2ce0e21e3325b329eedbe
StrangeMin <mim0107@nate.com>
2024-02-25 01:20:12 +0900

    [Feat] 카풀 상세페이지에서 요청하기 구현

--- AOS/app/src/main/java/org/softeer/robocar/ui/viewmodel/CarPoolDetailViewModel.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/viewmodel/CarPoolDetailViewModel.kt
@@ -3,11 +3,16 @@ package org.softeer.robocar.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.softeer.robocar.data.model.CarPool
+import org.softeer.robocar.domain.usecase.RequestCarPoolUseCase
 import javax.inject.Inject
 
+@HiltViewModel
 class CarPoolDetailViewModel @Inject constructor(
-    // 동승 신청 useCase 추가
+    private val requestCarPoolUseCase: RequestCarPoolUseCase
 ) : ViewModel() {
 
     private var _carPool = MutableLiveData<CarPool>()
@@ -19,6 +24,24 @@ class CarPoolDetailViewModel @Inject constructor(
     fun setCarPoolDetail(currentCarPool: CarPool, originalCharge: Int) {
         _carPool.value = currentCarPool
         _originalCharge.value = originalCharge
+    }
+
+    fun requestCarPool(
+        carPool: CarPool,
+        destinationLocation: String
+    ) {
+        viewModelScope.launch {
+            requestCarPoolUseCase(
+                carPool,
+                destinationLocation
+            )
+                .onSuccess {
+
+                }
+                .onFailure {
+
+                }
+        }
     }
 
 }
