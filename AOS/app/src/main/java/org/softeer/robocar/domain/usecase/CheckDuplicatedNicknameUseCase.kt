951ba5e9a139c581d54d73e2ead97b84f908f5b0
StrangeMin <mim0107@nate.com>
2024-03-13 05:41:39 +0900

    [Refactor] useCase에서 ApiResult를 반환하도록 수정

--- AOS/app/src/main/java/org/softeer/robocar/domain/usecase/CheckDuplicatedNicknameUseCase.kt
+++ AOS/app/src/main/java/org/softeer/robocar/domain/usecase/CheckDuplicatedNicknameUseCase.kt
@@ -1,6 +1,6 @@
 package org.softeer.robocar.domain.usecase
 
-import org.softeer.robocar.data.dto.user.response.CheckAvailableNicknameResponse
+import org.softeer.robocar.data.network.ApiResult
 import org.softeer.robocar.data.repository.user.UserRepository
 import javax.inject.Inject
 
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class CheckDuplicatedNicknameUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(nickname: String): Result<CheckAvailableNicknameResponse> {
+    suspend operator fun invoke(nickname: String): ApiResult {
         return userRepository.checkAvailableNickname(nickname)
     }
 }
--- AOS/app/src/main/java/org/softeer/robocar/domain/usecase/SignUpUseCase.kt
+++ AOS/app/src/main/java/org/softeer/robocar/domain/usecase/SignUpUseCase.kt
@@ -1,14 +1,14 @@
 package org.softeer.robocar.domain.usecase
 
 import org.softeer.robocar.data.dto.user.request.SignUpRequest
+import org.softeer.robocar.data.network.ApiResult
 import org.softeer.robocar.data.repository.user.UserRepository
 import javax.inject.Inject
 
 class SignUpUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(request: SignUpRequest): Boolean {
+    suspend operator fun invoke(request: SignUpRequest):ApiResult {
         return userRepository.signUp(request)
-            .isSuccess
     }
 }
