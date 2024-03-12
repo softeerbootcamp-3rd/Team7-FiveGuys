c236d95392a0e3c98adc4e9059d18461c065baa8
StrangeMin <mim0107@nate.com>
2024-03-13 05:50:18 +0900

    [Feat] 회원가입 시 네트워크 예외처리 구현

--- AOS/app/src/main/java/org/softeer/robocar/ui/adapter/SignUpBindingAdapter.kt
+++ AOS/app/src/main/java/org/softeer/robocar/ui/adapter/SignUpBindingAdapter.kt
@@ -7,14 +7,20 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import org.softeer.robocar.R
+import org.softeer.robocar.data.dto.user.response.CheckAvailableNicknameResponse
+import org.softeer.robocar.data.dto.user.response.CheckAvailableUserIdResponse
+import org.softeer.robocar.data.network.ApiResult
+import org.softeer.robocar.utils.showToast
 
 @BindingAdapter("activateButtonIfInputNotEmpty")
 fun activateButtonIfInputNotEmpty(button: AppCompatButton, input: String) {
     if (input.isNotEmpty()) {
-        button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_hyundai_blue_radius_12)
+        button.background =
+            ContextCompat.getDrawable(button.context, R.drawable.rectangle_hyundai_blue_radius_12)
         button.isClickable = true
     } else {
-        button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_600_radius_12)
+        button.background =
+            ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_600_radius_12)
         button.isClickable = false
     }
 }
@@ -33,52 +39,81 @@ fun activeButtonIfAllInputsComplete(
     password: String,
     confirmedPassword: String,
     nickname: String,
-    isAvailableId: Boolean,
-    isAvailableNickname: Boolean,
+    isAvailableId: ApiResult,
+    isAvailableNickname: ApiResult,
 ) {
     if (id.isNotEmpty() && password.isNotEmpty() && confirmedPassword.isNotEmpty() && nickname.isNotEmpty()
-        && isPasswordMatching(password, confirmedPassword) && isAvailableId && isAvailableNickname
+        && isPasswordMatching(
+            password,
+            confirmedPassword
+        ) && (isAvailableId is ApiResult.Success<*> && (isAvailableId.data as CheckAvailableUserIdResponse).isAvailable)
+        && isAvailableNickname is ApiResult.Success<*> && (isAvailableNickname.data as CheckAvailableNicknameResponse).isAvailable
     ) {
         button.isClickable = true
-        button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_hyundai_blue_radius_12)
+        button.background =
+            ContextCompat.getDrawable(button.context, R.drawable.rectangle_hyundai_blue_radius_12)
     } else {
-        button.background = ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_600_radius_12)
+        button.background =
+            ContextCompat.getDrawable(button.context, R.drawable.rectangle_gray_600_radius_12)
         button.isClickable = false
     }
 }
 
 @BindingAdapter("showIdAvailabilityMessage")
-fun showIdAvailabilityMessage(textView: AppCompatTextView, isAvailable: Boolean?) {
-    if (isAvailable == null)
-        return
+fun showIdAvailabilityMessage(textView: AppCompatTextView, idCheckState: ApiResult) {
+    when (idCheckState) {
+        is ApiResult.Loading -> {}
+        is ApiResult.Success<*> -> {
+            val isAvailable = (idCheckState.data as CheckAvailableUserIdResponse).isAvailable
+            showUserIdAvailabilityMessage(textView, isAvailable)
+        }
 
-    if (isAvailable) {
-        textView.text = "사용 가능한 아이디입니다."
-        textView.setTextColor(ContextCompat.getColor(textView.context, R.color.active_blue_pms_312C))
-    } else {
-        textView.text = "이미 존재하는 아이디입니다."
-        textView.setTextColor(Color.RED)
+        is ApiResult.Failure -> {
+            textView.context.showToast(
+                textView.context.getString(R.string.internal_server_error_toast_message)
+            )
+        }
+
+        is ApiResult.Exception -> {
+            textView.context.showToast(
+                textView.context.getString(R.string.network_error_toast_message)
+            )
+            textView.visibility = View.INVISIBLE
+        }
     }
-    textView.visibility = View.VISIBLE
 }
 
 @BindingAdapter("showNicknameAvailabilityMessage")
-fun showNicknameAvailabilityMessage(textView: AppCompatTextView, isAvailable: Boolean?) {
-    if (isAvailable == null)
-        return
+fun showNicknameAvailabilityMessage(textView: AppCompatTextView, nicknameCheckState: ApiResult) {
+    when (nicknameCheckState) {
+        is ApiResult.Loading -> {}
+        is ApiResult.Success<*> -> {
+            val isAvailable =
+                (nicknameCheckState.data as CheckAvailableNicknameResponse).isAvailable
+            showUserNicknameAvailabilityMessage(textView, isAvailable)
+        }
 
-    if (isAvailable) {
-        textView.text = "사용 가능한 닉네임입니다."
-        textView.setTextColor(ContextCompat.getColor(textView.context, R.color.active_blue_pms_312C))
-    } else {
-        textView.text = "이미 존재하는 닉네임입니다."
-        textView.setTextColor(Color.RED)
+        is ApiResult.Failure -> {
+            textView.context.showToast(
+                textView.context.getString(R.string.internal_server_error_toast_message)
+            )
+        }
+
+        is ApiResult.Exception -> {
+            textView.context.showToast(
+                textView.context.getString(R.string.network_error_toast_message)
+            )
+            textView.visibility = View.INVISIBLE
+        }
     }
-    textView.visibility = View.VISIBLE
 }
 
 @BindingAdapter("passwordMatching", "confirmedPasswordMatching")
-fun showPasswordMatchingMessage(textView: AppCompatTextView, password: String, confirmedPassword: String) {
+fun showPasswordMatchingMessage(
+    textView: AppCompatTextView,
+    password: String,
+    confirmedPassword: String
+) {
     if (confirmedPassword.isEmpty())
         return
 
@@ -91,4 +126,41 @@ fun showPasswordMatchingMessage(textView: AppCompatTextView, password: String, c
 
 private fun isPasswordMatching(password: String, confirmedPassword: String): Boolean {
     return password.equals(confirmedPassword)
+}
+
+private fun AppCompatTextView.showDuplicateCheckMessage(message: String, textColor: Int) {
+    text = message
+    setTextColor(textColor)
+    visibility = View.VISIBLE
+}
+
+private fun showUserIdAvailabilityMessage(textView: AppCompatTextView, isAvailableId: Boolean) {
+    if (isAvailableId) {
+        textView.showDuplicateCheckMessage(
+            textView.context.getString(R.string.is_available_id),
+            ContextCompat.getColor(textView.context, R.color.active_blue_pms_312C)
+        )
+    } else {
+        textView.showDuplicateCheckMessage(
+            textView.context.getString(R.string.is_already_exist_id),
+            Color.RED
+        )
+    }
+}
+
+private fun showUserNicknameAvailabilityMessage(
+    textView: AppCompatTextView,
+    isAvailableId: Boolean
+) {
+    if (isAvailableId) {
+        textView.showDuplicateCheckMessage(
+            textView.context.getString(R.string.is_available_nickname),
+            ContextCompat.getColor(textView.context, R.color.active_blue_pms_312C)
+        )
+    } else {
+        textView.showDuplicateCheckMessage(
+            textView.context.getString(R.string.is_already_exist_nickname),
+            Color.RED
+        )
+    }
 }
---
+++ AOS/app/src/main/java/org/softeer/robocar/utils/ContextExtensions.kt
@@ -0,0 +1,8 @@
+package org.softeer.robocar.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
