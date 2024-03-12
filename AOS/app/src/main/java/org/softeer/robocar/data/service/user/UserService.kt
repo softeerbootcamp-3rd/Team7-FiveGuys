f35a12f1ef0cbe75d6b46bb50805b90cc5d095fd
StrangeMin <mim0107@nate.com>
2024-03-13 05:15:23 +0900

    [Refactor] userService Response 타입으로 감싸도록 변경

--- AOS/app/src/main/java/org/softeer/robocar/data/service/user/UserService.kt
+++ AOS/app/src/main/java/org/softeer/robocar/data/service/user/UserService.kt
@@ -3,6 +3,7 @@ package org.softeer.robocar.data.service.user
 import org.softeer.robocar.data.dto.user.request.SignUpRequest
 import org.softeer.robocar.data.dto.user.response.CheckAvailableNicknameResponse
 import org.softeer.robocar.data.dto.user.response.CheckAvailableUserIdResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -13,15 +14,15 @@ interface UserService {
     @GET("users/loginId-validation")
     suspend fun checkAvailableUserId(
         @Query("loginId") userId: String
-    ): CheckAvailableUserIdResponse
+    ): Response<CheckAvailableUserIdResponse>
 
     @GET("users/nickname-validation")
     suspend fun checkAvailableNickname(
         @Query("nickname") nickname: String
-    ): CheckAvailableNicknameResponse
+    ): Response<CheckAvailableNicknameResponse>
 
     @POST("users")
     suspend fun signUp(
         @Body signUpRequest: SignUpRequest
-    )
+    ): Response<Unit>
 }
